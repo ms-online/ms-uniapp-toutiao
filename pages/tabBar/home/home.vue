42e6e4bba2875a0f393a393df32ee5486ba8b555
米斯特吴 <309595700@qq.com>
2020-05-18 16:47:51 +0800

    master

---
+++ pages/tabBar/home/home.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="container">
+		<homeHeader v-if="showHeader"></homeHeader>
+	</view>
+</template>
+
+<script>
+	import homeHeader from '../../../components/home/homeHeader.vue';
+	export default {
+		data() {
+			return {
+				showHeader: true, // 是否显示自定义表头
+			}
+		},
+		onLoad() {
+			// #ifdef APP-PLUS
+			this.showHeader = false;
+			// #endif
+		},
+		components: {
+			homeHeader
+		}
+	}
+</script>
+
+<style>
+</style>
